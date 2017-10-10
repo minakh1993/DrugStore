b42a43e03767073ee62108f971db81792e2d8506
fat <fatemeh.amirzargar@yahoo.com>
2017-10-10 15:40:58 +0330

    hibernate config

---
+++ src/javaClasses/purchase.java
@@ -0,0 +1,25 @@
+package javaClasses;
+
+public class purchase {
+
+	
+	
+	private int purchaseCode;
+	
+	private String name;
+	
+	
+	private String family;
+	
+	
+	private int AssuranceCode;
+	
+	
+	private double totalCost;
+	
+	
+	
+	
+	
+	
+}
